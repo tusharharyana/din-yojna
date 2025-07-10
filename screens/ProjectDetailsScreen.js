3ffcfc72ec60bd374d863138977f2bbac8fd2c43
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2025-07-10 11:51:50 +0530

    Seperate files created for css

--- screens/ProjectDetailsScreen.js
+++ screens/ProjectDetailsScreen.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   View,
   Text,
   FlatList,
   TextInput,
   TouchableOpacity,
-  StyleSheet,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Swipeable } from "react-native-gesture-handler";
+import styles from "../styles/detailsStyles";
+
 
 const ProjectDetailsScreen = ({ route }) => {
   const { project, updateProject } = route.params;
@@ -172,139 +173,5 @@ const ProjectDetailsScreen = ({ route }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    flex: 1,
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 8,
-  },
-  statusBadge: {
-    alignSelf: "flex-start",
-    paddingHorizontal: 12,
-    paddingVertical: 4,
-    borderRadius: 20,
-    marginBottom: 20,
-  },
-  greenBg: {
-    backgroundColor: "green",
-  },
-  orangeBg: {
-    backgroundColor: "orange",
-  },
-  statusText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  inputRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 16,
-  },
-  input: {
-    flex: 1,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 8,
-    padding: 10,
-    marginRight: 10,
-  },
-  addButton: {
-    backgroundColor: "#007AFF",
-    padding: 10,
-    borderRadius: 8,
-  },
-  taskItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#f9f9f9",
-    padding: 14,
-    marginBottom: 10,
-    borderRadius: 8,
-    borderLeftWidth: 4,
-    borderLeftColor: "#ccc",
-  },
-  completed: {
-    backgroundColor: "#d4f8c4",
-    borderLeftColor: "green",
-  },
-  taskText: {
-    fontSize: 16,
-    flex: 1,
-  },
-  taskTextCompleted: {
-    textDecorationLine: "line-through",
-    color: "gray",
-  },
-  emptyText: {
-    textAlign: "center",
-    color: "#888",
-    marginTop: 30,
-    fontSize: 16,
-  },
-  taskDeleteButton: {
-    backgroundColor: "red",
-    justifyContent: "center",
-    alignItems: "flex-end",
-    paddingHorizontal: 20,
-    marginBottom: 10,
-    borderRadius: 8,
-  },
-  taskDeleteText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  modalOverlay: {
-    position: "absolute",
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: "rgba(0,0,0,0.5)",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  modalContainer: {
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 10,
-    width: "80%",
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  modalButtons: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: 10,
-  },
-  cancel: {
-    color: "red",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  save: {
-    color: "green",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  renameInput: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 8,
-    padding: 10,
-    height: 40,
-    backgroundColor: "#f9f9f9",
-    fontSize: 16,
-    color: "#000",
-  },
-});
 
 export default ProjectDetailsScreen;
--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   View,
   Text,
   FlatList,
   TouchableOpacity,
-  StyleSheet,
   Modal,
   TextInput,
   Pressable,
@@ -13,6 +12,7 @@ import { AntDesign } from "@expo/vector-icons";
 import { useEffect } from "react";
 import { saveProjects, loadProjects } from "../utils/storage";
 import { Swipeable } from "react-native-gesture-handler";
+import styles from "../styles/projectsStyles";
 
 const initialProjects = [
   {
@@ -199,113 +199,5 @@ const ProjectsScreen = ({ navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    flex: 1,
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 16,
-  },
-  card: {
-    backgroundColor: "#fff",
-    padding: 16,
-    borderRadius: 10,
-    marginBottom: 12,
-    elevation: 3,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  status: {
-    marginTop: 4,
-    fontWeight: "bold",
-  },
-  green: {
-    color: "green",
-  },
-  orange: {
-    color: "orange",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 6,
-    padding: 10,
-    marginBottom: 10,
-  },
-  fab: {
-    position: "absolute",
-    bottom: 24,
-    right: 24,
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
-  },
-  modalContent: {
-    backgroundColor: "#fff",
-    marginHorizontal: 32,
-    padding: 20,
-    borderRadius: 10,
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  add: {
-    color: "green",
-    fontWeight: "bold",
-    fontSize: 16,
-    padding: 10,
-  },
-  cancel: {
-    color: "red",
-    fontSize: 16,
-    padding: 10,
-  },
-  emptyText: {
-    textAlign: "center",
-    marginTop: 40,
-    fontSize: 16,
-    color: "#888",
-  },
-  badge: {
-    paddingHorizontal: 10,
-    paddingVertical: 4,
-    borderRadius: 20,
-    alignSelf: "flex-start",
-    marginTop: 8,
-  },
-  badgeText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  greenBg: {
-    backgroundColor: "green",
-  },
-  orangeBg: {
-    backgroundColor: "orange",
-  },
-  deleteButton: {
-    backgroundColor: "red",
-    justifyContent: "center",
-    alignItems: "flex-end",
-    paddingHorizontal: 20,
-    marginBottom: 12,
-    borderRadius: 10,
-  },
-
-  deleteText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-});
 
 export default ProjectsScreen;
